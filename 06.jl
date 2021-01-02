5575952ec6ece9c6db22294dc2743ced17c6f4f6
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-01-02 22:39:38 +0100

    Add files via upload

--- 06.jl
+++ 06.jl
@@ -1,7 +1,7 @@
 open("06.txt") do f
     ls = readlines(f)
-    lights = BitArray(zeros(1000,1000))
-    lights2 = Array{Int}(zeros(1000,1000))
+    lights = falses(1000,1000)
+    lights2 = zeros(Int,1000,1000)
     for l ∈ ls
         ws = split(l)
         xl, yl = parse.(Int, split(ws[end-2],',')).+1
@@ -10,10 +10,10 @@ open("06.txt") do f
             lights[xl:xh,yl:yh] = .!lights[xl:xh,yl:yh]
             lights2[xl:xh,yl:yh] = lights2[xl:xh,yl:yh].+2
         elseif ws[2] == "off"
-            lights[xl:xh,yl:yh] = BitArray(zeros(xh-xl+1,yh-yl+1))
+            lights[xl:xh,yl:yh] = falses(xh-xl+1,yh-yl+1)
             lights2[xl:xh,yl:yh] = max.(lights2[xl:xh,yl:yh].-1, 0)
         else
-            lights[xl:xh,yl:yh] = BitArray(ones(xh-xl+1,yh-yl+1))
+            lights[xl:xh,yl:yh] = trues(xh-xl+1,yh-yl+1)
             lights2[xl:xh,yl:yh] =  lights2[xl:xh,yl:yh].+1
         end
     end
