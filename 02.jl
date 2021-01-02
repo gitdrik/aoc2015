1dad8acf38423d2ed041f150036189bc8a27521c
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-01-02 14:34:31 +0100

    Add files via upload

--- 02.jl
+++ 02.jl
@@ -1,7 +1,7 @@
 open("02.txt") do f
-    boxes = [sort(parse.(Int, split(l,'x'))) for l in eachline(f)]
+    boxes = [sort(parse.(Int, split(l,'x'))) for l ∈ eachline(f)]
     area(b) = 3b[1]b[2] + 2(b[1]b[3] + b[2]b[3])
-    println("Part 1: ", sum(area.(boxes)))
+    println("Part 1: ", mapreduce(area, +, boxes))
     ribbon(b) = 2(b[1] + b[2]) + prod(b)
-    println("Part 2: ", sum(ribbon.(boxes)))
+    println("Part 2: ", mapreduce(ribbon, +, boxes))
 end
