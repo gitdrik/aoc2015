13911e24c42738a2e9b890f5941e6bf33552019b
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-01-03 10:47:49 +0100

    Add files via upload

--- 05.jl
+++ 05.jl
@@ -1,23 +1,11 @@
 open("05.txt") do f
     ls = readlines(f)
-    has3wovf(ss) = filter(s->length([c for c in s if c ∈ Set(['a','e','i','o','u'])]) > 2, ss)
+    has3wovf(ss) = filter(s->length([c for c ∈ s if c ∈ Set(['a','e','i','o','u'])]) > 2, ss)
     hasrepf(ss) = filter(s->mapreduce(a->a[1]==a[2], |, zip(s,s[2:end])), ss)
-    has0badf(ss) = filter(s->mapreduce(a->string(a...) ∉ Set(["ab", "cd", "pq", "xy"]), &, zip(s,s[2:end])), ss)
+    has0badf(ss) = filter(s->any(occursin(a,s) for a ∈ ["ab", "cd", "pq", "xy"]), ss)
     println("Part 1: ", ls |> has3wovf |> hasrepf |> has0badf |> length)
 
-    function hasrep2(s)
-        for i ∈ 1:length(s)-3, j ∈ i+2:length(s)-1
-            s[i:i+1] == s[j:j+1] && return true
-        end
-        return false
-    end
-    function hasx_x(s)
-        for i ∈ 1:length(s)-2
-            s[i] == s[i+2] && return true
-        end
-        return false
-    end
-    hasrep2f(ss) = filter(s->hasrep2(s), ss)
-    hasx_xf(ss) = filter(s->hasx_x(s), ss)
+    hasrep2f(ss) = filter(s->any(s[i:i+1]==s[j:j+1] for i ∈ 1:length(s)-3 for j ∈ i:length(s)-1), ss)
+    hasx_xf(ss) = filter(s->any(s[i]==s[i+2] for i ∈ 1:length(s)-2), ss)
     println("Part 2: ", ls |> hasrep2f |> hasx_xf |> length)
 end
