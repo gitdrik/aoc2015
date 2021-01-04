af3699e79314648eca873d96daa856ecd74228e0
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-01-04 23:55:30 +0100

    Add files via upload

---
+++ 10.jl
@@ -0,0 +1,24 @@
+function multisaylength(look, n)
+    function say(look)
+        isempty(look) && return look
+        seq, i, last = [], 1, look[1]
+        for n in look[2:end]
+            if n==last
+                i +=1
+                continue
+            end
+            push!(seq,i,last)
+            i, last = 1, n
+        end
+        push!(seq,i,last)
+        return seq
+    end
+
+    for i ∈ 1:n
+        look = say(look)
+    end
+    return length(look)
+end
+seed = [1,3,2,1,1,3,1,1,1,2]
+println("Part 1: ", multisaylength(seed, 40))
+println("Part 2: ", multisaylength(seed, 50))
