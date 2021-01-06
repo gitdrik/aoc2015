316d36e2165c22aa8cfc2cfdc1308c19d7e79497
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-01-06 21:27:34 +0100

    Add files via upload

---
+++ 13.jl
@@ -0,0 +1,41 @@
+using DataStructures
+using Combinatorics
+
+open("13.txt") do f
+    prefs = DefaultDict{String,Dict{String, Int}}(Dict())
+    for l ∈ eachline(f)
+        ss = split(strip(l,'.'))
+        prefs[ss[1]][ss[11]] = ss[3]=="lose" ? -parse(Int,ss[4]) : parse(Int,ss[4])
+    end
+
+    function allseatings(guests)
+        # take one random guest
+        remaining = collect(guests)
+        g1 = pop!(remaining)
+        # place every remaining pair around, order not important
+        threes = [[g2, g1, g3] for (g2,g3) ∈ combinations(remaining,2)]
+        # seat all remaining permutations
+        seatings = []
+        for three ∈ threes
+            rests = collect(setdiff(guests, g for g ∈ three))
+            for rest ∈ permutations(rests)
+                push!(seatings, [three;rest])
+            end
+        end
+        return seatings
+    end
+
+    function happiestseating(prefs)
+        guests, n = Set(keys(prefs)), length(keys(prefs))
+        seatings = allseatings(guests)
+        maximum([sum([prefs[s[i]][s[mod1(i-1,n)]] + prefs[s[i]][s[mod1(i+1,n)]]
+                   for i ∈ 1:n]) for s ∈ seatings])
+    end
+    println("Part 1: ", happiestseating(prefs))
+
+    for g ∈ keys(prefs)
+        prefs[g]["myself"] = 0
+        prefs["myself"][g] = 0
+    end
+    println("Part 2: ", happiestseating(prefs))
+end
